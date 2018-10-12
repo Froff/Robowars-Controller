78cfd01b0f218f2ca9f00d27a642d35f9126367a
Froff <froff@protonmail.com>
2018-10-13 01:21:44 +0200

    Made LED blinking work over radio

--- controller/controller.ino
+++ controller/controller.ino
@@ -2,85 +2,19 @@
 #include "RF24.h"
 #include "codesheet.h"
 #include "math.h"
-#include <LiquidCrystal.h>
 
 // Pins ************************************************************************
-#define BUTTON_PIN A5
-#define X_AXIS_PIN A7
-#define Y_AXIS_PIN A6
-#define LCDRS 8
-#define LCDEN 7
-#define LCDD4 6
-#define LCDD5 5
-#define LCDD6 4
-#define LCDD7 3
 #define RADIOCE 9
 #define RADIOCS 10
 #define RADIO_INTERRUPT_PIN 2
-
-#define ARM_DOWN_PIN A0
-#define ARM_UP_PIN A1
+#define SWITCH_PIN 3
 
 // Constants *******************************************************************
-#define JOYSTICK_DEADZONE 0.05
-
 byte writeAdress[6] = "Pingu";
 byte readAdress[6] = "Singu";
 
-// Class declarations **********************************************************
-class Joystick {
-public:
-    double getX() {double r = joystickSlerp(x, minx, maxx); return abs(r) < JOYSTICK_DEADZONE ? 0 : r;}
-    double getY() {double r = joystickSlerp(y, miny, maxy); return abs(r) < JOYSTICK_DEADZONE ? 0 : r;}
-    bool getSW() {return sw;}
-
-    Joystick (int xPin, int yPin, int swPin) {
-        this->xPin = xPin;
-        this->yPin = yPin;
-        this->swPin = swPin;
-        pinMode(xPin, INPUT);
-        pinMode(yPin, INPUT);
-        pinMode(swPin, INPUT_PULLUP);
-    }
-
-    void readPositions() {
-        x = analogRead(xPin);
-        y = analogRead(yPin);
-        sw = digitalRead(swPin) == LOW;
-        calibrate();
-    }
-
-private:
-    int xPin, yPin, swPin;
-
-    int x = 512;
-    int y = 512;
-    bool sw = false;
-
-    int minx = 1024;
-    int maxx = 0;
-    int miny = 1024;
-    int maxy = 0;
-
-    void calibrate() {
-        minx = min(minx, x);
-        maxx = max(maxx, x);
-        miny = min(miny, y);
-        maxy = max(maxy, y);
-    }
-
-    // Slerps a numbers x between min and max to a number between -1 and 1
-    double joystickSlerp (int t, int min, int max) {
-        return 2*(double(t) - 0.5* (double(max) - double(min))) / (double(max) - double(min));
-    }
-};
-
 // Globals *********************************************************************
-Joystick joy (X_AXIS_PIN, Y_AXIS_PIN, BUTTON_PIN);
 RF24 radio(RADIOCE, RADIOCS);
-LiquidCrystal lcd (LCDRS, LCDEN, LCDD4, LCDD5, LCDD6, LCDD7);
-int lcd_draw_countdown = 0;
-const int lcd_draw_countdown_start = 1;
 
 // Function declarations *******************************************************
 void sendSignal(const uint32_t & value);
@@ -99,45 +33,21 @@ void setup() {
     radio.openWritingPipe(writeAdress);
     radio.openReadingPipe(1, readAdress);
 
-    // Pin setup
-    pinMode(ARM_UP_PIN, INPUT_PULLUP);
-    pinMode(ARM_DOWN_PIN, INPUT_PULLUP);
-
-    // LCD setup
-    lcd.begin(16,2);
-    lcd.print("penor bol");
-
-    // Settinginterrupts
+    // Settinginterrupts*/
     pinMode(RADIO_INTERRUPT_PIN, INPUT_PULLUP);
     delay(50);
     attachInterrupt(0, radioInterrupt, LOW);             // Attach interrupt handler to interrupt #0 (using pin 2) on BOTH the sender and receiver
+
+    // Switch setup
+    pinMode(SWITCH_PIN, INPUT_PULLUP);
 
     delay(50);
 }
 
 void loop() {
-    joy.readPositions();
-    uint32_t message= composeAndSendMessage();
-
-    if (lcd_draw_countdown <= 0) {
-        writeLCDjoysticks();
-        lcd_draw_countdown = lcd_draw_countdown_start;
-    }
-    lcd_draw_countdown--;
-
+    uint32_t message = composeAndSendMessage();
     Serial.println(message, BIN);
     delay(100);
-}
-
-void writeLCDjoysticks () {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("joyx ");
-    lcd.print(joy.getX());
-    lcd.setCursor(0,1);
-    lcd.print("joyy ");
-    lcd.print(joy.getY());
-    if (joy.getSW()) lcd.print("pOOsh");
 }
 
 uint32_t composeAndSendMessage () {
@@ -148,36 +58,9 @@ uint32_t composeAndSendMessage () {
 
 uint32_t composeMessage () {
     uint32_t message = 0;
-
-    // Check buttons, and add result to message (0 false, 1 true)
-    if (joy.getSW()) { message |= CODE_BEAK_NOOT_BITMASK; }
-    if (digitalRead(ARM_UP_PIN) == LOW) { message |= CODE_ARM_UP; }
-    if (digitalRead(ARM_DOWN_PIN) == LOW) { message |= CODE_ARM_DOWN; }
-
-    // Check joysticks
-    joy.readPositions();
-    double left_wheel_acc = 0.0;
-    double right_wheel_acc = 0.0;
-    left_wheel_acc  += (joy.getY() + joy.getX())/2;
-    right_wheel_acc += (joy.getY() - joy.getX())/2;
-
-    // Bound joystick outputs
-    if (left_wheel_acc < -0.99) left_wheel_acc = -0.99;
-    else if (left_wheel_acc > 0.99) left_wheel_acc = 0.99;
-    if (right_wheel_acc < -0.99) right_wheel_acc = -0.99;
-    else if (right_wheel_acc > 0.99) right_wheel_acc = 0.99;
-
-    // Create wheel bits
-    uint32_t left_wheel_input  = ((uint32_t)floor(abs(8 * left_wheel_acc))) & WHEEL_BITMASK_UNSHIFTED;
-    left_wheel_input |= left_wheel_acc < 0 ? 0b1000 : 0b0000;
-    left_wheel_input <<= WHEEL_LEFT_BITSHIFT;
-
-    uint32_t right_wheel_input = ((uint32_t)floor(abs(8 * right_wheel_acc))) & WHEEL_BITMASK_UNSHIFTED;
-    right_wheel_input |= right_wheel_acc < 0 ? 0b1000 : 0b0000;
-    right_wheel_input <<= WHEEL_RIGHT_BITSHIFT;
-
-    message |= left_wheel_input | right_wheel_input;
-
+    if (digitalRead(SWITCH_PIN) == HIGH) {
+        message = 1;
+    }
     return message;
 }
 
--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -4,16 +4,8 @@
 #include <stdint.h>
 
 // Pins
-#define COM_BEAK_NOOT_PIN A2
-#define COM_BEAK_VOLUP_PIN A1
-#define COM_BEAK_VOLDOWN_PIN A0
-#define ARM_PWM_UP 3
-#define ARM_PWM_DOWN 4
-#define RIGHT_WHEEL_PWM_FORW 5
-#define LEFT_WHEEL_PWM_FORW 6
-#define RIGHT_WHEEL_PWM_BACK 7
-#define LEFT_WHEEL_PWM_BACK 8
-
+#define LED_PIN 3
+#define RADIO_INTERRUPT_PIN 2
 
 // Constants
 byte writeAdress[6] = "Singu";
@@ -24,7 +16,6 @@ const uint32_t ack = 1;
 RF24 radio(9,10);
 uint32_t lastmessage = 0;
 bool message_in_queue = false;
-
 
 // Main
 void setup() {
@@ -43,21 +34,13 @@ void setup() {
     radio.writeAckPayload(1, &ack, sizeof(uint32_t));
     Serial.println(F("Finished radio setup"));
 
-    // Pin setup
-    pinMode(COM_BEAK_NOOT_PIN, OUTPUT); digitalWrite(COM_BEAK_NOOT_PIN, HIGH);
-    pinMode(COM_BEAK_VOLUP_PIN, OUTPUT); digitalWrite(COM_BEAK_VOLUP_PIN, HIGH);
-    pinMode(COM_BEAK_VOLDOWN_PIN, OUTPUT); digitalWrite(COM_BEAK_VOLDOWN_PIN, HIGH);
-    pinMode(LEFT_WHEEL_PWM_FORW, OUTPUT); digitalWrite(LEFT_WHEEL_PWM_FORW, LOW);
-    pinMode(LEFT_WHEEL_PWM_BACK, OUTPUT); digitalWrite(LEFT_WHEEL_PWM_BACK, LOW);
-    pinMode(RIGHT_WHEEL_PWM_FORW, OUTPUT); digitalWrite(RIGHT_WHEEL_PWM_FORW, LOW);
-    pinMode(RIGHT_WHEEL_PWM_BACK, OUTPUT); digitalWrite(RIGHT_WHEEL_PWM_BACK, LOW);
-    pinMode(ARM_PWM_UP, OUTPUT); digitalWrite(ARM_PWM_UP, LOW);
-    pinMode(ARM_PWM_DOWN, OUTPUT); digitalWrite(ARM_PWM_DOWN, LOW);
-
     // radio interrupt
-    pinMode(2, INPUT_PULLUP);
+    pinMode(RADIO_INTERRUPT_PIN, INPUT_PULLUP);
     delay(50);
     attachInterrupt(0, catchMessage, LOW);
+
+    // Pin
+    pinMode(LED_PIN, OUTPUT);
 
     Serial.println(F("Finished setup"));
 }
@@ -80,55 +63,12 @@ void catchMessage() {
 
 // Handles the incoming message. Returns true iff signal was recognized
 bool handleMessage(int32_t message_in) {
-    // Set pins
-    if (message_in & CODE_BEAK_NOOT_BITMASK) {
-        Serial.println(F("Doin' the noot noot!"));
-        digitalWrite(COM_BEAK_NOOT_PIN, LOW);
-    } else digitalWrite(COM_BEAK_NOOT_PIN, HIGH);
-
-    if (message_in & CODE_ARM_UP) {
-        Serial.println(F("moving arm up"));
-        digitalWrite(ARM_PWM_UP, HIGH);
-        digitalWrite(ARM_PWM_DOWN, LOW);
-    } else digitalWrite(ARM_PWM_UP, LOW);
-
-    if (message_in & CODE_ARM_DOWN) {
-        Serial.println(F("moving arm down"));
-        digitalWrite(ARM_PWM_DOWN, HIGH);
-        digitalWrite(ARM_PWM_UP, LOW);
-    } else digitalWrite(ARM_PWM_DOWN, LOW);
-
-        Serial.print("ARM-UP high? ");
-        Serial.println(digitalRead(ARM_PWM_UP) == HIGH);
-            Serial.print("ARM-DOWN high? ");
-            Serial.println(digitalRead(ARM_PWM_DOWN) == HIGH);
-
-    uint32_t left = (message_in >> WHEEL_LEFT_BITSHIFT);
-    if (left & WHEEL_BITMASK_UNSHIFTED) {
-        if (left & 0b1000) {
-            Serial.println(F("moving left wheel forwards"));
-            digitalWrite(LEFT_WHEEL_PWM_BACK, HIGH);
-        } else {
-            Serial.println(F("moving left wheel back"));
-            digitalWrite(LEFT_WHEEL_PWM_FORW, HIGH);
-        }
+    Serial.print(F("Message: "));
+    Serial.println(message_in);
+    if (message_in == 1) {
+        digitalWrite(LED_PIN, HIGH);
     } else {
-        digitalWrite(LEFT_WHEEL_PWM_FORW, LOW);
-        digitalWrite(LEFT_WHEEL_PWM_BACK, LOW);
-    }
-
-    uint32_t right = (message_in >> WHEEL_RIGHT_BITSHIFT);
-    if (right & WHEEL_BITMASK_UNSHIFTED) {
-        if (right & 0b1000){
-            Serial.println(F("moving right wheel forwards"));
-            digitalWrite(RIGHT_WHEEL_PWM_BACK, HIGH);
-        } else {
-            Serial.println(F("moving right wheel back"));
-            digitalWrite(RIGHT_WHEEL_PWM_FORW, HIGH);
-        }
-    } else {
-        digitalWrite(RIGHT_WHEEL_PWM_FORW, LOW);
-        digitalWrite(RIGHT_WHEEL_PWM_BACK, LOW);
+        digitalWrite(LED_PIN, LOW);
     }
     return true;
 }
